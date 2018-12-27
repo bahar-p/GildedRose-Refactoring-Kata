5227f4ba5b49f9d4585329226ebadfe51358dcde
Bahar <pourazar.bahare@gmail.com>
2018-12-27 15:44:54 -0500

    refactor, added module for items

---
+++ ruby/gilded_rose_items.rb
@@ -0,0 +1,8 @@
+module GildedRoseItems
+
+  AGED_BRIE = 'Aged Brie'.freeze
+  BS_CONCERT = 'Backstage passes to a TAFKAL80ETC concert'.freeze
+  SULFURAS = 'Sulfuras, Hand of Ragnaros'.freeze
+  CONJURED = 'Conjured'.freeze
+
+end
